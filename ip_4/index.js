262a62a96110da6ca6d1a67cf909598c9546aa6d
gwashh <perry.sugut@student.moringaschool.com>
2021-11-30 15:56:30 +0300

    new changes

--- ip_4/index.js
+++ ip_4/index.js
@@ -1,21 +1,23 @@
 function IP4(){
-    var size = document.getElementById("size").selected;
+    var size = document.getElementById("size").value;
     var crust = document.getElementById("crust").value;
     var toppings = document.getElementById("topping").value;
     var number = document.getElementById("number").value;
     var pizzas =parseInt(number);
+    const price = document.getElementById("price")
 
-    alert("hello")
+    
 
-    if(size == "small"){
+    if(size == "Small"){
         size = 450;
+    
         
     }
-    else if(size == "medium"){
+    else if(size == "Medium"){
         size = 750;
     
     }
-    else if(size == "large"){
+    else if(size == "Large"){
         size = 1400;
     
     }
@@ -24,15 +26,30 @@ function IP4(){
     }
 
 
-    if(crust == "crispy"){
+    if(crust == "Crispy"){
         crust = 50;
     }
-    if(crust == "stuffed"){
+    if(crust == "Stuffed"){
         crust = 150;
     }
-    if(crust == "gluten free"){
+    if(crust == "Gluten-Free"){
         crust = 200;
     }
+    
+
+    if(topping == "Sausage"){
+        topping = 70;
+    }
+    if(topping == "Mushrooms"){
+        topping = 70;
+    }
+    if(topping == "Extra Cheese"){
+        topping = 70;
+    }
+
+
+    finalPrice = size + crust + toppings
+    price.innerHTML = ` <h1>Total Price: Ksh ${finalPrice}</h1>`
     
 
 
@@ -41,5 +58,7 @@ function IP4(){
 
 
 
-}
 
+}
+const order = document.getElementById("order")
+order.addEventListener("click", IP4)
